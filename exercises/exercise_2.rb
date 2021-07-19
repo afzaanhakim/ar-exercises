f827179cec21461ec5b31f4a3783b8329d91c41f
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-07-19 21:09:03 +0000

    exercise 2 - update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Updated Store"
+
+puts @store1.inspect
+puts @store2.inspect
