3cfdf439e07d531a4c673d58d4714ba7e8b50186
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-07-19 23:18:45 +0000

    add validations for employees and stores class and implement exercise_7 to create store and display error message

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please put a store name"
+new_name = gets.chomp
+
+
+ new = Store.create(name: new_name)
+
+ puts new.inspect
+
+ new.errors.each do |error, message| 
+  puts "this is err: #{error}", "this is message: #{message}"
+ end
